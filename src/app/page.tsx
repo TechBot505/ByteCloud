ee202fa31611ff60d3ce71bdbf2840d76cbde9b3
Rohit <rohitdhanotia2003@gmail.com>
2024-08-09 15:59:31 +0530

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   SignInButton,
@@ -5,8 +6,14 @@ import {
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+
+  const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedOut>
@@ -19,6 +26,14 @@ export default function Home() {
           <Button>Sign Out</Button>
         </SignOutButton>
       </SignedIn>
+
+      {files?.map(file => {
+        return <div key={file._id}>{file.name}</div>
+      })}
+
+      <Button onClick={() => {
+        createFile({ name: 'Hello World' });
+      }}>Click Me</Button>
     </main>
   );
 }
