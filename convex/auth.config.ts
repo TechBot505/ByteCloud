f66277cd2d267b1f4730b925c8f11ed91ceebe98
Rohit <rohitdhanotia2003@gmail.com>
2024-08-09 15:34:09 +0530

    Implemented Clerk Auth and Added Convex Backend API

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://ample-penguin-35.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+    </main>
+  );
+}
